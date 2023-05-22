70bfe860460216612ec86ffd80c9ac7f47583af2
Apolosmllb <miner@quadrant.pe>
2023-05-21 22:53:28 -0500

    add running-sum-of-1d-array solution

--- hackerRank/between-two-sets.js
+++ hackerRank/between-two-sets.js
@@ -1,18 +1,46 @@
-const array1 = {
-  a: 1,
-  b: 2,
-  z: 1,
+const getTotalX = (a, b) => {
+  //console.log(a, b);
+  let mcm = [];
+  let divisor = 0;
+  let divide = 2;
+  let multi = 1
+  a.forEach((a) => {
+    while (a >= divide) {
+      console.log(`entrando a/div: ${a}/${divide}`);
+      if (a % divide === 0 && divisor !== divide) {
+        console.log("~ divisor:", divisor);
+        if (a / divide === 1) divisor = divide;
+        console.log("~ divisor:", divisor);
+        console.log(`Sí Cumple a/div: ${a}/${divide}`);
+        
+        mcm.forEach(e => {
+          multi = multi * e
+         console.log('element ', e);
+        });
+        console.log('multi:', multi)
+        mcm.push(divide)
+      } else {
+        console.log(`----No cumple a/div: ${a}/${divide}`);
+      }
+      divide++;
+    }
+    multi = 1
+    divide = 2
+  });
+  console.log("~ mcm:", mcm);
 };
 
-a = [2, 6];
+a = [2, 6, 5];
 b = [24, 36];
 
+getTotalX(a, b);
 
+// console.log("a / divide:", a / divide)
 
+/*console.log(`${a}`, '/', `${divide}`)
+a % divide === 0 && (a / divide !== 1 ) ? mcm.push(divide) : null;
+*/
 
-console.log("array1 -->", Object.keys(array1));
-
-
-
-a = [1,2];
-b = [1,2,3];
+/*  a % divide === 0
+  ? console.log(`Sí Cumple a/div: ${a}/${divide}`)
+  : console.log(`----No cumple a/div: ${a}/${divide}`);*/
---
+++ let-code/Running-Sum-of-1d-Array.js
@@ -0,0 +1,29 @@
+/**
+ * Running-Sum-of-1d-Array.js
+ * */
+
+
+//0(n) TC 
+//0(1) EClet results = [];
+  results[0] = nums[0];
+  for (let i = 1; i < nums.length; i++) results[i] = nums[i] + results[i - 1];
+  return results;
+const runningSum = (nums) => {
+  let results = [];
+  results[0] = nums[0];
+  for (let i = 1; i < nums.length; i++) results[i] = nums[i] + results[i - 1];
+  return results;
+};
+
+//0(n) TC 
+//0(1) EC
+const runningSum2 = (nums) => {
+  for (let i = 1; i < nums.length; i++) nums[i] += nums[i - 1];
+  return nums;
+};
+
+const a = [1, 2, 3, 4];
+const b = [1, 2, 3, 4];
+
+runningSum2(a);
+runningSum(b);
