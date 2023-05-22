e36507a3fc1dd87e7798bfcd2a08b305ec56587e
Apolosmllb <miner@quadrant.pe>
2023-05-21 22:55:44 -0500

    fix running-sum-of-1d-array solution

--- let-code/Running-Sum-of-1d-Array.js
+++ let-code/Running-Sum-of-1d-Array.js
@@ -4,10 +4,7 @@
 
 
 //0(n) TC 
-//0(1) EClet results = [];
-  results[0] = nums[0];
-  for (let i = 1; i < nums.length; i++) results[i] = nums[i] + results[i - 1];
-  return results;
+//0(1) EC
 const runningSum = (nums) => {
   let results = [];
   results[0] = nums[0];
